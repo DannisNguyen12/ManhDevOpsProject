016eda3576cbcb9fdff60a779dbbf576f93c77be
AyeshaOmer <ayesha@skipq.org>
2025-09-30 11:51:02 +1000

    after cleanup

--- Ayesha-Pipelines/modules/constants.py
+++
@@ -1,4 +0,0 @@
-URL_TO_MONITOR = "https://www.skipq.org"
-URL_MONITOR_NAMESPACE = "AyeshaWebHealthProject"
-URL_MONITOR_METRIC_NAME_AVAILABILITY = "url_availability"
-URL_MONITOR_METRIC_NAME_LATENCY= "url_latency"
