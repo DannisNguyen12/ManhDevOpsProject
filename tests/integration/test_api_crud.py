efdc3165d854907adc30ec4337c16ad4a2bd5626
Dan Nguyen <long.nm187254@gmail.com>
2025-10-21 09:23:07 +1100

    Try

--- tests/integration/test_api_crud.py
+++ tests/integration/test_api_crud.py
@@ -14,10 +14,3 @@ def test_create_website_validation():
     invalid_website = {"name": "Test Site", "url": "invalid-url"}
     error = validate_website(invalid_website)
     assert error == "URL must start with http:// or https://"
-
-
-def test_list_websites_empty():
-    """Test listing websites when none exist"""
-    # This is a simple test that doesn't require AWS resources
-    websites = []
-    assert len(websites) == 0
